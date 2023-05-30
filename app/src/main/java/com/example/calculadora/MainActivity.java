7cf22e420ac7a902b8b9c9e3d45947ad7bb09a27
VladimirChandi <ljemri184@gmail.com>
2023-05-29 22:54:08 -0500

    He cambiado algunas cosas en el codigo para tratar de que sea un poco mas responsivo :)

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SpeechRecognizer speechRecognizer;
+    private EditText activeEditText;
+
+
     EditText et1, et2;
     TextView res;
     Button Sumar, Resta, Multi, Divi;
@@ -18,6 +44,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+
+            public void onReadyForSpeech(Bundle params) {
+                // El reconocimiento de voz está listo
+            }
+
+
+            public void onBeginningOfSpeech() {
+                // Comienza el habla del usuario
+            }
+
+
+            public void onRmsChanged(float rmsdB) {
+                // Cambios en el nivel de sonido
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+
+            public void onResults(Bundle results) {
+                // Resultados del reconocimiento de voz
+                ArrayList<String> speechResults = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (speechResults != null && !speechResults.isEmpty() && activeEditText != null) {
+                    String spokenText = speechResults.get(0);
+                    activeEditText.setText(spokenText);
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+
+
+        });
+
+
         setContentView(R.layout.activity_main);
         et1 = (EditText) findViewById(R.id.et1);
         et2 = (EditText) findViewById(R.id.et2);
@@ -105,4 +187,14 @@ public class MainActivity extends AppCompatActivity {
    finish();
 
   }
+    private void startListening() {
+        activeEditText = et1; // Establece el campo de texto activo al EditText et1
+        speechRecognizer.startListening(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH));
+    }
+
+    private void stopListening() {
+        activeEditText = null; // No se requiere un campo de texto activo
+        speechRecognizer.stopListening();
+    }
+
 }
